91ab6a895042054ba98501e16a8fa4913848318f
Mitsuhau Emoto <e-cas@ezweb.ne.jp>
2019-01-18 23:14:05 +0900

    Initial Commit

---
+++ MEOException/MEOException.h
@@ -0,0 +1,19 @@
+//
+//  MEOException.h
+//  MEOException
+//
+//  Created by Mitsuhau Emoto on 2019/01/18.
+//  Copyright © 2019 Mitsuhau Emoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MEOException.
+FOUNDATION_EXPORT double MEOExceptionVersionNumber;
+
+//! Project version string for MEOException.
+FOUNDATION_EXPORT const unsigned char MEOExceptionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MEOException/PublicHeader.h>
+
+
